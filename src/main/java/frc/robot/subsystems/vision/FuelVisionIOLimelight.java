1f295ba77dc55d5415ed709eed80e6fe8bcf074a
mdm314 <mafdm314159@gmail.com>
2026-02-03 19:41:23 -0700

    using 2024 as a base

--- src/main/java/frc/robot/subsystems/vision/FuelVisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/vision/FuelVisionIOLimelight.java
@@ -12,4 +12,72 @@ public class FuelVisionIOLimelight implements FuelVisionIO {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
     
+    private final String name;
+    private final Mat homography =
+            Calib3d.findHomography(
+                    new MatOfPoint2f( // Camera points
+                            new Point(177, 134),
+                            new Point(469, 132),
+                            new Point(532, 270),
+                            new Point(132, 266)),
+                    new MatOfPoint2f( // Real world points
+                            new Point(Units.inchesToMeters(22 + 15), Units.inchesToMeters(-4)),
+                            new Point(Units.inchesToMeters(22 + 15), Units.inchesToMeters(5)),
+                            new Point(Units.inchesToMeters(52 + 15), Units.inchesToMeters(20)),
+                            new Point(Units.inchesToMeters(54 + 15), Units.inchesToMeters(-18))));
+
+    public FuelVisionIOLimelight(String limelightName) {
+        this.name = limelightName;
+    }
+
+    @Override
+    public void updateInputs(FuelVisionInputs inputs) {
+        LimelightHelpers.LimelightResults results = LimelightHelpers.getLatestResults(name);
+
+        inputs.timestampCapture = 0;
+
+        DoubleArrayEntry jsonEntity = LimelightHelpers.getLimelightDoubleArrayEntry(name, "t2d");
+
+        var tsValue = jsonEntity.getAtomic();
+
+        double adjustedTimestamp =
+                (tsValue.timestamp / 1000000.0)
+                        - ((results.latency_capture + results.latency_pipeline) / 1000.0);
+
+        inputs.timestampCapture = adjustedTimestamp;
+
+        if (results.targets_Detector.length < 1) {
+            inputs.valid = false;
+            return;
+        }
+        LimelightHelpers.LimelightTarget_Detector detectorResults = results.targets_Detector[0];
+
+        inputs.tx = -detectorResults.tx;
+        inputs.ty = -detectorResults.ty;
+        inputs.valid = results.valid;
+
+        inputs.txPixels = detectorResults.tx_pixels;
+        inputs.tyPixels = detectorResults.ty_pixels;
+
+        Mat src =
+                new MatOfPoint2f(
+                        new Point(
+                                inputs.txPixels,
+                                inputs.tyPixels)); // Matrix of vectors to be transformed
+        MatOfPoint2f dst =
+                new MatOfPoint2f(); // A mutable matrix for opencv to store thebotspace coordinates;
+
+        try {
+            Core.perspectiveTransform(src, dst, homography);
+            inputs.projectionValid = true;
+        } catch (Exception e) {
+            System.out.println("OpenCV Homography transformation failed");
+            inputs.projectionValid = false;
+        }
+
+        Point fuelBotSpace = dst.toArray()[0];
+
+        inputs.botSpaceX = fuelBotSpace.x;
+        inputs.botSpaceY = fuelBotSpace.y;
+    }
 }
