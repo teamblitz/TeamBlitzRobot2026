94930c4b865c29319a1cbde790b43d9b0901b5f8
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2026-04-01 17:32:41 -0600

    swerve works -- needs tuning

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 98;
-  public static final String GIT_SHA = "3b420161857b45fc7dd1fb02aeb349f8387c537b";
-  public static final String GIT_DATE = "2026-04-01 11:53:56 EDT";
-  public static final String GIT_BRANCH = "dev-advantageKit";
-  public static final String BUILD_DATE = "2026-04-01 17:39:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1775079579857L;
+  public static final int GIT_REVISION = 99;
+  public static final String GIT_SHA = "c14e205b99787556c6e0223c540ce0f24e9532a2";
+  public static final String GIT_DATE = "2026-04-01 17:41:33 EDT";
+  public static final String GIT_BRANCH = "dev-advantageKit-v1-development";
+  public static final String BUILD_DATE = "2026-04-01 19:24:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1775085866451L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
