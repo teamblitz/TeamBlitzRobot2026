febc8a5cffc421a9d081f4f0e6fa7df6812aacae
mdm314 <mafdm314159@gmail.com>
2026-04-01 18:11:09 -0600

    need vendor dep

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 100;
-  public static final String GIT_SHA = "94930c4b865c29319a1cbde790b43d9b0901b5f8";
-  public static final String GIT_DATE = "2026-04-01 19:32:41 EDT";
+  public static final int GIT_REVISION = 101;
+  public static final String GIT_SHA = "a973e4c232bb143a6a9143c3fa7823350473cae1";
+  public static final String GIT_DATE = "2026-04-01 20:06:30 EDT";
   public static final String GIT_BRANCH = "dev-advantageKit-v1-development";
-  public static final String BUILD_DATE = "2026-04-01 20:04:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1775088296896L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-04-01 20:08:13 EDT";
+  public static final long BUILD_UNIX_TIME = 1775088493471L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
