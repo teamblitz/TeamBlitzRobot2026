477ad9895f99bae8123379ceebc5e3583359fa4e
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2026-04-01 18:58:50 -0600

    auto stopping before cmd() finishes on StraightTest

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 103;
-  public static final String GIT_SHA = "97584415170209e796a43b3c99866f71977ceeee";
-  public static final String GIT_DATE = "2026-04-01 20:16:41 EDT";
+  public static final int GIT_REVISION = 104;
+  public static final String GIT_SHA = "7a527714b85fd6e8b7b61b93c2e2e329c945bac2";
+  public static final String GIT_DATE = "2026-04-01 20:27:03 EDT";
   public static final String GIT_BRANCH = "dev-advantageKit-v1-development";
-  public static final String BUILD_DATE = "2026-04-01 20:26:55 EDT";
-  public static final long BUILD_UNIX_TIME = 1775089615592L;
+  public static final String BUILD_DATE = "2026-04-01 20:48:45 EDT";
+  public static final long BUILD_UNIX_TIME = 1775090925731L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
