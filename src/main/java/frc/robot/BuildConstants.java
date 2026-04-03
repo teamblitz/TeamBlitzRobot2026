0cd9b4ad1e7c0eae926fddcef7d5c9c5dc0b2693
FRC Team Blitz #2083 <github@teamblitz.net>
2026-04-02 18:30:15 -0600

    path setup. speed need to be fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 104;
-  public static final String GIT_SHA = "7a527714b85fd6e8b7b61b93c2e2e329c945bac2";
-  public static final String GIT_DATE = "2026-04-01 20:27:03 EDT";
+  public static final int GIT_REVISION = 105;
+  public static final String GIT_SHA = "477ad9895f99bae8123379ceebc5e3583359fa4e";
+  public static final String GIT_DATE = "2026-04-01 20:58:50 EDT";
   public static final String GIT_BRANCH = "dev-advantageKit-v1-development";
-  public static final String BUILD_DATE = "2026-04-01 20:48:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1775090925731L;
+  public static final String BUILD_DATE = "2026-04-02 20:24:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1775175874854L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
