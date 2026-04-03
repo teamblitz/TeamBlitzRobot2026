1a37dc50b274acf1f427a14556ccd55c11779aac
mdm314 <mafdm314159@gmail.com>
2026-04-03 16:08:04 -0600

    old wrist moved over. worth testing before trying the existing double motor setup.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 101;
-  public static final String GIT_SHA = "feeeb1b7cb0b60653aee37a4aa81603fd2597653";
-  public static final String GIT_DATE = "2026-04-02 21:32:37 EDT";
-  public static final String GIT_BRANCH = "dev-v2-advantageKit";
-  public static final String BUILD_DATE = "2026-04-02 21:46:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1775180798082L;
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "3d4faac40486ea22a35be8cf0c604b702d668ff6";
+  public static final String GIT_DATE = "2026-04-02 21:57:48 EDT";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2026-04-03 18:06:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1775253984588L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/subsystems/wrist/WristIO.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.wrist;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface WristIO {
+
+  @AutoLog
+  public static class WristInputs {
+    public double rpm;
+    public double current;
+    public double absoluteEncoderPosition;
+    public double velocityRadiansPerSecond;
+  }
+
+  default void updateInputs(WristInputs inputs) {}
+
+  default void setSpeed(double speed) {}
+
+  default void setMotionMagic(double position) {}
+
+  default void stop() {}
+}
