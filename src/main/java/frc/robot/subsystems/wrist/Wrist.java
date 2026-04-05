99326b89517620e389447955442af4e95f04fcfe
Matthew <mafdm314159@gmail.com>
2026-04-05 14:43:40 -0600

    reverted back to old wrist

--- src/main/java/frc/robot/subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -3,21 +3,30 @@ package frc.robot.subsystems.wrist;
 import static frc.robot.Constants.WristConstants.*;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.BlitzSubsystem;
 import java.util.Optional;
+import java.util.function.DoubleSupplier;
+import frc.robot.subsystems.wrist.WristIOKraken;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.AutoLogOutput;
+
 
 public class Wrist extends BlitzSubsystem {
   private final WristIO io;
-  private final WristInputsAutoLogged inputs = new WristInputsAutoLogged();
-  private Optional<Double> goal = Optional.empty();
 
   public Wrist(WristIO io) {
     super("Wrist");
     this.io = io;
+    goal = Optional.empty();
   }
+
+  private final WristInputsAutoLogged inputs = new WristInputsAutoLogged();
+
+  private Optional<TrapezoidProfile.State> goal;
 
   @Override
   public void periodic() {
@@ -25,15 +34,26 @@ public class Wrist extends BlitzSubsystem {
     io.updateInputs(inputs);
     Logger.processInputs(logKey, inputs);
 
-    // if (goal.isPresent() && DriverStation.isEnabled()) {
-    //     System.out.println("Sending Motion Magic to: " + goal.get());
-    //     io.setMotionMagic(goal.get());
-    // }
+    if (goal.isPresent() && DriverStation.isEnabled()) {
+      System.out.println("Sending Motion Magic to: " + goal.get().position);
+      io.setMotionMagic(goal.get().position);
+    }
 
     if (DriverStation.isDisabled()) {
       goal = Optional.empty();
       io.stop();
     }
+
+
+
+  }
+
+  public Command upTest() {
+    return startEnd(() -> io.setSpeed(0.5), () -> io.setSpeed(0));
+  }
+
+  public Command setSpeed(double speed) {
+    return startEnd(() -> io.setSpeed(speed), () -> io.setSpeed(0));
   }
 
   public Command goToIdle() {
@@ -41,26 +61,33 @@ public class Wrist extends BlitzSubsystem {
   }
 
   public Command goToDown() {
-    return goToPosition(EXTENDED_POS);
+    return followGoal(EXTENDED_POS);
   }
 
-  public Command goToPosition(Double position) {
-    return runOnce(
-        () -> {
-          // this is not really needed but here for example
-          boolean near = MathUtil.isNear(position, getPosition(), TOLERANCE);
-          if (near) return; // exit if its already there
+  public Command goToPosition(double position) {
+    return followGoal(position)
+            .withDeadline(
+                    Commands.waitUntil(() -> {
+                      boolean near = MathUtil.isNear(position, getPosition(), TOLERANCE);
+                      System.out.println("position: " + getPosition() + " target: " + position + " near: " + near);
+                      return near;
+                    }))
+            .withName(logKey + "/goToPosition_waitForMechanism " + position);
+  }
 
-          // sets the goal to keep it around in case we want to log or use it in periodic.
-          if (this.goal.isEmpty() || this.goal.get() != position) {
-            this.goal = Optional.of(position);
-          }
-
-          // System.out.println("Finished goToPosition with position:" + position);
-
-          io.setMotionMagic(position);
-          System.out.println("Finished goToPosition with position:" + position);
-        });
+  public Command followGoal(double goal) {
+    return run(() -> {
+      if (this.goal.isEmpty() || this.goal.get().position != goal) {
+        System.out.println("**************Setting goal to " + goal);
+        this.goal = Optional.of(new TrapezoidProfile.State(
+                MathUtil.clamp(goal, Math.min(EXTENDED_POS, IDLE_POS), Math.max(EXTENDED_POS, IDLE_POS)), 0));
+      }
+    })
+            .handleInterrupt(() -> {
+              System.out.println("Interrupted, goal was: " + this.goal.map(s -> String.valueOf(s.position)).orElse("empty"));
+              this.goal = Optional.empty();
+            })
+            .beforeStarting(() -> System.out.println("followGoal starting"));
   }
 
   public double getPosition() {
@@ -70,69 +97,4 @@ public class Wrist extends BlitzSubsystem {
   public double getVelocity() {
     return inputs.velocityRadiansPerSecond;
   }
-
-  // public Command goToPositionOld(double position) {
-  //     return followGoal(position)
-  //             .withDeadline(
-  //                     Commands.waitUntil(() -> {
-  //                         boolean near = MathUtil.isNear(position, getPosition(), TOLERANCE);
-  //                         System.out.println("position: " + getPosition() + " target: " +
-  // position + " near: " + near);
-  //                         return near;
-  //                     }))
-  //             .withName(logKey + "/goToPosition_waitForMechanism " + position);
-  // }
-
-  // public Command followGoal(double goal) {
-  //     return run(() -> {
-  //         if (this.goal.isEmpty() || this.goal.get().position != goal) {
-  //             System.out.println("**************Setting goal to " + goal);
-  //             this.goal = Optional.of(new TrapezoidProfile.State(
-  //                     MathUtil.clamp(goal, Math.min(EXTENDED_POS, IDLE_POS),
-  // Math.max(EXTENDED_POS, IDLE_POS)), 0));
-  //         }
-  //     })
-  //             .handleInterrupt(() -> {
-  //                 System.out.println("Interrupted, goal was: " + this.goal.map(s ->
-  // String.valueOf(s.position)).orElse("empty"));
-  //                 this.goal = Optional.empty();
-  //             })
-  //             .beforeStarting(() -> System.out.println("followGoal starting"));
-  // }
-
-  //    private Command refreshCurrentState() {
-  //        return runOnce(() -> setpoint = new TrapezoidProfile.State(getPosition(),
-  // getVelocity()))
-  //                .onlyIf(() -> setpoint == null || goal.isEmpty());
-  //    }
-
-  /* public Command move_up() {
-      // return startEnd(() -> io.setMotionMagic(1.64), () -> io.setSpeed(0));
-      return Commands.runOnce(() -> {
-          io.setMotionMagic(1);
-          System.out.println("Finishes move");
-      });
-  }
-
-  public Command move_down() {
-      // return startEnd(() -> io.setSpeed(-0.3), () -> io.setSpeed(0));
-      return Commands.runOnce(() -> {
-          io.setMotionMagic(0);
-      });
-
-  } */
-  // Do not use
-
-  // public Command upTest() {
-  //     return startEnd(() -> io.setSpeed(0.5), () -> io.setSpeed(0));
-  // }
-
-  //     @AutoLogOutput(key = "wrist/idealPosition")
-  //     public double getIdealPosition() {
-  // //        if (goal.isPresent()) {
-  // //            return setpoint.position;
-  // //        }
-  //         return getPosition();
-  //     }
-
 }
--- src/main/java/frc/robot/subsystems/wrist/WristIOKraken.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOKraken.java
@@ -6,141 +6,102 @@ import static frc.robot.Constants.WristConstants.INVERTED;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.*;
+
+import edu.wpi.first.math.MathUtil;
 import org.littletonrobotics.junction.Logger;
 
 public class WristIOKraken implements WristIO {
 
-  /*   Motors   */
-  public final TalonFX rightWrist;
-  public final TalonFX leftWrist;
+  public final TalonFX wrist;
+  public final CANcoder absoluteEncoder;
 
-  /*   Absolute Encoders   */
-  public final CANcoder rightAbsoluteEncoder;
-  public final CANcoder leftAbsoluteEncoder;
-
-  private final MotionMagicVoltage motionMagicRequest =
-      new MotionMagicVoltage(0).withSlot(0).withEnableFOC(true);
+  private final MotionMagicVoltage motionMagicRequest = new MotionMagicVoltage(0).withSlot(0).withEnableFOC(true);
   private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true);
   private final NeutralOut neutralRequest = new NeutralOut();
-  private final Follower followerRequest;
 
   public WristIOKraken() {
-    rightWrist = new TalonFX(18); // TODO set
-    leftWrist = new TalonFX(20); // TODO set
-    rightAbsoluteEncoder = new CANcoder(19); // TODO set
-    leftAbsoluteEncoder = new CANcoder(21); // TODO set
+    wrist = new TalonFX(21);
+    absoluteEncoder = new CANcoder(ABS_ENCODER_ID);
 
-    /*   Motor Synchronization   */
-    followerRequest =
-        new Follower(
-            leftWrist.getDeviceID(), MotorAlignmentValue.Aligned); // May need to be opposed
-
-    /*   Right Absolute Encoder Config   */
     CANcoderConfiguration encoderConfig = new CANcoderConfiguration();
-    encoderConfig.MagnetSensor.SensorDirection =
-        SensorDirectionValue.CounterClockwise_Positive; // May need to be Clockwise_Positive
-    encoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1; // TODO retune
-    encoderConfig.MagnetSensor.MagnetOffset = -0.8; // TODO retune
-    rightAbsoluteEncoder.getConfigurator().apply(encoderConfig);
+    encoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+    encoderConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.9;
 
-    /*   Left Absolute Encoder Config   */
-    CANcoderConfiguration encoderConfig2 = new CANcoderConfiguration();
-    encoderConfig2.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
-    encoderConfig2.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1;
-    encoderConfig2.MagnetSensor.MagnetOffset = -0.154; // TODO retune
-    leftAbsoluteEncoder.getConfigurator().apply(encoderConfig2);
+    // Set this to the negated raw reading when wrist is at the zero position
+    encoderConfig.MagnetSensor.MagnetOffset = MAGNET_OFFSET;
+    absoluteEncoder.getConfigurator().apply(encoderConfig);
 
-    /*   Right Side Motor - Primary Motor   */
     TalonFXConfiguration config = new TalonFXConfiguration();
+
     config.CurrentLimits.StatorCurrentLimit = CURRENT_LIMIT_WRIST;
     config.CurrentLimits.StatorCurrentLimitEnable = true;
-    config.MotorOutput.withNeutralMode(NeutralModeValue.Coast)
-        .withInverted(
-            INVERTED ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive);
-    config.Feedback.FeedbackRemoteSensorID = rightAbsoluteEncoder.getDeviceID();
-    config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+
+    config.MotorOutput
+            .withNeutralMode(NeutralModeValue.Brake)
+            .withInverted(
+                    INVERTED
+                            ? InvertedValue.Clockwise_Positive
+                            : InvertedValue.CounterClockwise_Positive);
+
+    config.Feedback.FeedbackRemoteSensorID = absoluteEncoder.getDeviceID();
+    config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;  //Fused if we want pro
+    // config.Feedback.SensorToMechanismRatio = SENSOR_TO_MECHANISM_RATIO;
     config.Feedback.RotorToSensorRatio = ROTOR_TO_SENSOR_RATIO;
+
+    // PID gains for Motion Magic (slot 0)
     config.Slot0.kP = KP;
     config.Slot0.kI = KI;
     config.Slot0.kD = KD;
     config.Slot0.kG = KG;
     config.Slot0.kV = KV;
     config.Slot0.kS = KS;
-    config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+
     config.MotionMagic.MotionMagicCruiseVelocity = MAX_VELOCITY;
     config.MotionMagic.MotionMagicAcceleration = MAX_ACCEL;
     config.MotionMagic.MotionMagicJerk = 0;
-    config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
-    config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = SOFT_LIMIT_FORWARD;
-    config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
-    config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = SOFT_LIMIT_REVERSE;
-    rightWrist.getConfigurator().apply(config);
 
-    /*   Left Side Motor - Follower, may not need gains   */
-    TalonFXConfiguration config2 = new TalonFXConfiguration();
-    config2.CurrentLimits.StatorCurrentLimit = CURRENT_LIMIT_WRIST;
-    config2.CurrentLimits.StatorCurrentLimitEnable = true;
-    config2.MotorOutput.withNeutralMode(NeutralModeValue.Coast)
-        .withInverted(
-            INVERTED ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive);
-    leftWrist.getConfigurator().apply(config2);
+    config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+    // config.Slot0.GravityArmPositionOffset = IDLE_POS;
+    config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = SOFT_LIMIT_FORWARD; // small buffer past zero
+    config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = SOFT_LIMIT_REVERSE; // small buffer before zero
+
+    wrist.getConfigurator().apply(config);
   }
 
   @Override
   public void updateInputs(WristInputs inputs) {
-    //   Applying the follower control again to ensure that the left motor follower is correctly
-    // applied
-    leftWrist.setControl(followerRequest);
+    inputs.velocityRadiansPerSecond = wrist.getVelocity().getValueAsDouble();
+    inputs.absoluteEncoderPosition = getAbsPosition();
+    inputs.current = wrist.getStatorCurrent().getValueAsDouble();
 
-    boolean primaryOk = rightAbsoluteEncoder.getAbsolutePosition().getStatus().isOK();
-    double position =
-        primaryOk
-            ? rightAbsoluteEncoder.getAbsolutePosition().getValueAsDouble()
-            : leftAbsoluteEncoder.getAbsolutePosition().getValueAsDouble();
-
-    inputs.velocityRadiansPerSecond = rightWrist.getVelocity().getValueAsDouble();
-    inputs.absoluteEncoderPosition = position;
-    inputs.current = rightWrist.getStatorCurrent().getValueAsDouble();
-
-    Logger.recordOutput("wrist/motorPosition", rightWrist.getPosition().getValueAsDouble());
-    Logger.recordOutput("wrist/absoluteEncoderPosition", position);
-    Logger.recordOutput(
-        "wrist/encoder1Position", rightAbsoluteEncoder.getAbsolutePosition().getValueAsDouble());
-    Logger.recordOutput(
-        "wrist/encoder2Position", leftAbsoluteEncoder.getAbsolutePosition().getValueAsDouble());
-    Logger.recordOutput("wrist/usingPrimaryEncoder", primaryOk);
-    Logger.recordOutput(
-        "wrist/motionMagicRunning", rightWrist.getMotionMagicIsRunning().getValue());
-    Logger.recordOutput("wrist/motor1Current", rightWrist.getStatorCurrent().getValueAsDouble());
-    Logger.recordOutput("wrist/motor2Current", leftWrist.getStatorCurrent().getValueAsDouble());
-  }
+    Logger.recordOutput("wrist/motorPosition", wrist.getPosition().getValueAsDouble());
+    Logger.recordOutput("wrist/absoluteEncoderPosition", getAbsPosition());
+    Logger.recordOutput("wrist/motionMagicEnabled", wrist.getMotionMagicIsRunning().getValue());    }
 
   @Override
   public void setSpeed(double speed) {
-    rightWrist.set(speed);
+    wrist.set(speed);
   }
 
   @Override
   public void setMotionMagic(double position) {
-    rightWrist.setControl(motionMagicRequest.withPosition(position));
+    wrist.setControl(motionMagicRequest.withPosition(position));
   }
 
   @Override
   public void stop() {
-    rightWrist.setControl(neutralRequest);
+    wrist.setControl(neutralRequest);
   }
 
   public double getAbsPosition() {
-    boolean primaryOk = rightAbsoluteEncoder.getAbsolutePosition().getStatus().isOK();
-    return primaryOk
-        ? rightAbsoluteEncoder.getAbsolutePosition().getValueAsDouble()
-        : leftAbsoluteEncoder.getAbsolutePosition().getValueAsDouble();
+    return absoluteEncoder.getAbsolutePosition().getValueAsDouble();
   }
 }
