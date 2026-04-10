f3f4fd51f73cf98db55141c246173dd9c3c76f6b
FRC Team Blitz #2083 <github@teamblitz.net>
2026-04-10 17:04:12 -0600

    fix Current Limits

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -81,7 +81,7 @@ public final class Constants {
     public static final double MAX_ACCEL = 0.75; // TODO set
 
     public static final double CURRENT_LIMIT_WRIST =
-        60; // may need to be adjusted for further testing
+        30; // may need to be adjusted for further testing
 
     // Left
     public static final double LEFT_KP = 45; // TODO tune
@@ -113,7 +113,7 @@ public final class Constants {
   public static final class Intake {
     public static final int INTAKE_ID = 17; // TODO set val
 
-    public static final double CURRENT_LIMIT = 120; // TODO set val
+    public static final double CURRENT_LIMIT = 30; // TODO set val
 
     public static final boolean INVERTED = true; // may need to be set to false
   }
