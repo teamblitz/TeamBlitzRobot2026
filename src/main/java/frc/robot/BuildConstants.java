cc89e09e12e6fde82016daaf1606010880319719
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2026-04-11 08:34:58 -0600

    Add unstick command

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 153;
-  public static final String GIT_SHA = "8cb0d64fa21b076a9764aaf44751c242bec14e20";
-  public static final String GIT_DATE = "2026-04-10 20:18:18 EDT";
+  public static final int GIT_REVISION = 154;
+  public static final String GIT_SHA = "c34d005640861c113ba8518a2e49a29ed35f7fb2";
+  public static final String GIT_DATE = "2026-04-11 10:11:38 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-04-11 10:10:36 EDT";
-  public static final long BUILD_UNIX_TIME = 1775916636708L;
+  public static final String BUILD_DATE = "2026-04-11 10:34:02 EDT";
+  public static final long BUILD_UNIX_TIME = 1775918042614L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
