627187a6c955ee0527233fd4bf871217468d64ad
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2026-05-12 19:50:25 -0600

    Fixing autos
    Generated code doesn't save to the code. We still need to fix this issue but copy pasting is a temporary fix.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlitzRobot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 171;
-  public static final String GIT_SHA = "091fc3abc00be0b1bed4ac8e65b97e07b9c3c45d";
-  public static final String GIT_DATE = "2026-05-05 20:41:47 EDT";
+  public static final int GIT_REVISION = 173;
+  public static final String GIT_SHA = "2e0c6bd33232e22bdeea5e3ff2e2938a0a08f7cd";
+  public static final String GIT_DATE = "2026-05-12 20:53:54 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-05-05 21:12:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1778029957914L;
+  public static final String BUILD_DATE = "2026-05-12 21:48:36 EDT";
+  public static final long BUILD_UNIX_TIME = 1778636916450L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
